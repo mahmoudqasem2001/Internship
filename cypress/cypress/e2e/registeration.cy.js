f83c7ef98896750c90b881456e082518ab445d6b
mahmoud khaled <mahmoodqasem543@gmail.com>
2022-08-22 12:02:40 +0300

    make some changes

--- cypress/cypress/e2e/registeration.cy.js
+++ cypress/cypress/e2e/registeration.cy.js
@@ -1,15 +1,15 @@
 /// <reference types="cypress" />
+
 import {
-  locators,
-  visitURL,
-  click,
-  type,
+  LOCATORS,
+  goToRegistrationPage,
+  inputTextField,
   checkEmailMessageError,
-  checkURL,
-  checkRedColorError,
+  checkUrl,
+  verifyTextFieldColorChanged,
   fillOptionalFields,
-  checkIfErrorShown,
-  checkErrorTypeVisable,
+  verifyAlertShown,
+  verifyAlertErrorMessage,
   fillRequiredFields
 } from '../support/registration_helper';
 
@@ -21,76 +21,80 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe('Registeration page', () => {
 
   before('go to sign-in page', () => {
-    visitURL('http://automationpractice.com/index.php')
-    click(locators.sign_in_btn)
+    goToRegistrationPage("http://automationpractice.com/index.php")
   })
 
   it('Registration with invalid Email', () => {
     //--------Email without "@" char-----------//
-    type(locators.email, 'sampleUser4gmail.com')
-    click(locators.submitEmail_btn)
+    inputTextField(LOCATORS.email, 'sampleUser4gmail.com')
+    click(LOCATORS.submitEmail_btn)
     checkEmailMessageError();
     //--------Email without dot --------------//
-    type(locators.email, 'sampleUser4@gmailcom')
+    inputTextField(LOCATORS.email, 'sampleUser4@gmailcom')
     checkEmailMessageError();
-    click(locators.submitEmail_btn)
+    click(LOCATORS.submitEmail_btn)
     //----------Email without name------//
-    type(locators.email, '@gmail.com')
+    inputTextField(LOCATORS.email, '@gmail.com')
     checkEmailMessageError();
-    click(locators.submitEmail_btn)
+    click(LOCATORS.submitEmail_btn)
   })
 
-  it('Registration with valid Email', () => {
-    type(locators.email, 'sampleUser7@gmail.com')
-    click(locators.submitEmail_btn)
-    checkURL('http://automationpractice.com/index.php?controller=authentication&back=my-account#account-creation')
+  it.only('Registration with valid Email', () => {
+    inputTextField(LOCATORS.email, 'sampleUser7@gmail.com')
+    cy.get(LOCATORS.submitEmailBtn)
+      .click({force: true})
+    checkUrl('http://automationpractice.com/index.php?controller=authentication&back=my-account#account-creation')
   })
 
   it('Verify Clicking on required fields without filling them', () => {
-    click(locators.firstName)
+    click(LOCATORS.firstName)
     click('body')
-    checkRedColorError(locators.firstName)
+    checkRedColorError(LOCATORS.firstName)
+    
 
-
-    click(locators.lastName)
+    click(LOCATORS.lastName)
     click('body')
-    checkRedColorError(locators.lastName)
+    checkRedColorError(LOCATORS.lastName)
 
-    click(locators.password)
+    click(LOCATORS.password)
     click('body')
-    checkRedColorError(locators.password)
+    checkRedColorError(LOCATORS.password)
 
   })
 
   it('Registration with invalid phone number ', () => {
-    type(locators.mobile_phone, 'abcd')
-    click(locators.submitAccount_btn)
-    checkIfErrorShown()
-    checkErrorTypeVisable(locators.phone_number)
+    inputTextField(LOCATORS.mobile_phone, 'abcd')
+    click(LOCATORS.submitAccount_btn)
+    verifyAlertShown()
+    checkErrorinputTextFieldVisable(LOCATORS.phone_number)
   })
 
-  it('Registration with invalid password', () => {
-    type(locators.password, '123')
-    click(locators.submitAccount_btn)
-    checkIfErrorShown()
-    checkErrorTypeVisable(locators.password)
+  it.only('Registration with invalid password', () => {
+    inputTextField(LOCATORS.password, '123445678910111211314151617181920212223')
+    cy.get(LOCATORS.submitAccountBtn)
+      .click({force: true})    
+    verifyAlertShown()
+    verifyAlertErrorMessage(LOCATORS.password,'123445678910111211314151617181920212223')
+    
+    inputTextField(LOCATORS.password, '123')
+    cy.get(LOCATORS.submitAccountBtn)
+      .click({force: true})    
+    verifyAlertShown()
+    verifyAlertErrorMessage(LOCATORS.password,'123')
 
-    type(locators.password, '123445678910111211314151617181920212223')
-    click(locators.submitAccount_btn)
-    checkIfErrorShown()
-    checkErrorTypeVisable(locators.password)
+    
   })
 
   it('Registration with invalid postal code', () => {
-    type(locators.postalCode, '1234567')
-    click(locators.submitAccount_btn)
-    checkIfErrorShown()
-    checkErrorTypeVisable(locators.postalCode)
+    inputTextField(LOCATORS.postalCode, '1234567')
+    click(LOCATORS.submitAccount_btn)
+    verifyAlertShown()
+    checkErrorinputTextFieldVisable(LOCATORS.postalCode)
   })
 
   it('Registration without filling required fields', () => {
-    click(locators.submitAccount_btn)
-    checkIfErrorShown()
+    click(LOCATORS.submitAccount_btn)
+    verifyAlertShown()
   })
 
   it('Registration without filling required fields and filling optional fields', () => {
@@ -101,8 +105,8 @@ describe('Registeration page', () => {
       'Exalt',
       'no other description',
       '0595129072')
-    click(locators.submitAccount_btn)
-    checkIfErrorShown()
+    click(LOCATORS.submitAccount_btn)
+    verifyAlertShown()
   })
 
   it('Registration with filling required fields', () => {
@@ -119,7 +123,7 @@ describe('Registeration page', () => {
       '0599344870',
       'milan'
       )
-    click(locators.submitAccount_btn)
+    click(LOCATORS.submitAccount_btn)
     checkURL('http://automationpractice.com/index.php?controller=my-account')
     })
 
--- cypress/cypress/support/registration_helper.js
+++ cypress/cypress/support/registration_helper.js
@@ -1,187 +1,142 @@
-const locators = {
-    signIn_page_url: 'http://automationpractice.com/index.php',
-    registerPage_url: 'http://automationpractice.com/index.php?controller=authentication&back=my-account#account-creation',
-    submitEmail_btn: '#SubmitCreate > span',
-    sign_in_btn: '.login',
-    submitAccount_btn: '#submitAccount > span',
+/// <reference types="cypress" />
+
+export const URLS = {
+    SIGNIN_PAGE_URL: 'http://automationpractice.com/index.php',
+    REGISTER_PAGE_URL: 'http://automationpractice.com/index.php?controller=authentication&back=my-account#account-creation'
+}
+export const ERROR_MESSAGES={
+    MISS_FILLING_PHONE_NUMBER: "You must register at least one phone number.",
+    MISS_FILLING_LAST_NAME: "lastname is required.",
+    MISS_FILLING_LAST_NAME: "firstname is required.",
+    MISS_FILLING_PASSWORD: "passwd is required.",
+    MISS_FILLING_ADDRESS1: "address1 is required.",
+    MISS_FILLING_CITY: "city is required.",
+    WRONG_POSTAL_CODE: "The Zip/Postal code you've entered is invalid. It must follow this format: 00000",
+    MISS_SHOOSE_STATE: "This country requires you to choose a State",
+    SMALL_PASSWORD:"passwd is invalid.",
+    LARGE_PASSWORD:"passwd is too long. Maximum length: 32",
+    INVALID_EMAIL: "Invalid email address"
+}
+export const COLORS={
+    RED_ERROR:"rgb(241, 51, 64)"
+}
+export const LOCATORS = {
+    submitEmailBtn: '#SubmitCreate > span',
+    signInBtn: '.login',
+    submitAccountBtn: '#submitAccount > span',
     email: '#email_create',
     firstName: '#customer_firstname',
     lastName: '#customer_lastname',
     password: '#passwd',
-    phone_number: '#phone_mobile',
-    errorMessage: '.alert',
+    alert: '.alert',
     postalCode: '#postcode',
-    days: '#days',
-    months: '#months',
-    years: '#years',
-    newsLetter_checkbox: '#newsletter',
-    offer_checkbox: '#optin',
-    company_name: '#company',
+    daysMenuList: '#days',
+    monthsMenuList: '#months',
+    yearsMenuList: '#years',
+    newsLetterCheckbox: '#newsletter',
+    offerCheckbox: '#optin',
+    companyName: '#company',
     otherDiscription: '#other',
     homePhone: '#phone',
     address1: '#address1',
     address2: '#address2',
     city: '#city',
-    state_Id: '#id_state',
-    country_Id: '#id_country',
-    mobile_phone: '#phone_mobile',
-    alias_address: '#alias',
+    stateId: '#id_state',
+    countryId: '#id_country',
+    mobilePhone: '#phone_mobile',
+    aliasAddress: '#alias',
 }
-const visitURL = (url) => {
+export const goToRegistrationPage = (url) => {
     cy.visit(url)
+    cy.get(LOCATORS.signInBtn)
+      .click({force: true})
 }
-const click = (locator) => {
-    cy.get(locator)
-        .click({ force: true })
-}
-const checkEmailMessageError = () => {
+//can make it general??
+export const checkEmailMessageError = (inputLocator) => {
     cy.wait(10000)
-    cy.get(locators.email)
-        .should('have.css', 'color', 'rgb(241, 51, 64)')
-    cy.get(locators.errorMessage)
-        .should('be.visible', { timeout: 100000 })
+    cy.get(inputLocator)
+      .should('have.css', 'color', COLORS.RED_ERROR)
+    cy.get(LOCATORS.alert)
+      .should('be.visible', { timeout: 100000 })
     cy.get('li')
-        .contains('Invalid email address')
-        .should('be.visible')
+      .contains(ERROR_MESSAGES.INVALID_EMAIL)
+      .should('be.visible')
 }
-const type = (locator, text) => {
-    cy.get(locator)
-        .clear({ force: true })
-        .type(text)
-        .should('have.value', text)
+export const inputTextField = (textFieldLocator, text) => {
+    cy.get(textFieldLocator)
+      .clear({ force: true })
+      .type(text)
+      .should('have.value', text)
 }
 
-const checkURL = (url) => {
+export const checkUrl = (url) => {
     cy.url({ timeout: 20000 })
-        .should('eq', url)
+      .should('eq', url)
 
 }
-const checkRedColorError = (locator) => {
-    cy.get(locator)
-        .should('have.css', 'color', 'rgb(241, 51, 64)')
+export const verifyTextFieldColorChanged = (textFieldLocator) => {
+    cy.get(textFieldLocator)
+      .should('have.css', 'color', COLORS.RED_ERROR)
 }
-const fillOptionalFields = (
-    day,
-    month,
-    year,
-    company,
-    otherDiscription,
-    homePhone,
-) => {
-    cy.get(locators.days)
-        .select(day, { force: true })
-        .should('have.value', day)
-    cy.get(locators.months)
-        .select(month, { force: true })
-        .should('have.value', month)
-    cy.get(locators.years)
-        .select(year, { force: true })
-        .should('have.value', year)
-    cy.get(locators.newsLetter_checkbox)
-        .check({ force: true })
-        .should('be.checked')
-    cy.get(locators.offer_checkbox)
-        .check({ force: true })
-        .should('be.checked')
-    cy.get(locators.company_name)
-        .clear({ force: true })
-        .type(company)
-        .should('have.value', company)
-    cy.get(locators.otherDiscription)
-        .clear({ force: true })
-        .type(otherDiscription)
-        .should('have.value', otherDiscription)
-    cy.get(locators.homePhone)
-        .clear({ force: true })
-        .type(homePhone)
-        .should('have.value', homePhone)
+export const selectFromList=(listLocator,value)=>{
+    cy.get(listLocator)
+      .select(value, { force: true })
+      .should('have.value', value)
+}
+export const checkbox=(checkboxLocator)=>{
+    cy.get(checkboxLocator)
+      .check({ force: true })
+      .should('be.checked')
+}
+export const fillOptionalFields = (day, month, year, companyName, otherDiscription, homePhone ) => {
+    selectFromList(LOCATORS.daysMenuList,day)
+    selectFromList(LOCATORS.monthsMenuList,month)
+    selectFromList(LOCATORS.yearsMenuList,year)
+    
+    checkbox(LOCATORS.offerCheckbox)
+    checkbox(LOCATORS.newsLetterCheckbox)
 
+    inputTextField(LOCATORS.company_name,companyName)
+    inputTextField(LOCATORS.otherDiscription,otherDiscription)
+    inputTextField(LOCATORS.homePhone)
 }
-const checkIfErrorShown = () => {
-    cy.get(locators.errorMessage)
+export const verifyAlertShown = () => {
+    cy.get(LOCATORS.alert)
         .should('exist')
         .should('be.visible')
 }
-const checkErrorTypeVisable = (locator) => {
-    if (locator == "#passwd") {
+export const verifyAlertErrorMessage = (inputLocator) => {
+    if (inputLocator === LOCATORS.password) {
         cy.get('li')
-            .contains('passwd')
-            .should('be.visible')
+          .contains('passwd')       //How I specify if it long passwd or small??  
+          .should('be.visible')
+        
     }
-    else if (locator == "#postcode") {
+    else if (inputLocator === LOCATORS.postalCode) {
         cy.get('li')
-            .contains("The Zip/Postal code you've entered is invalid. It must follow this format: 00000")
-            .should('be.visible')
+          .contains(ERROR_MESSAGES.WRONG_POSTAL_CODE)
+          .should('be.visible')
     }
-    else if (locator == "#phone_mobile") {
+    else if (inputLocator === LOCATORS.mobilePhone) {
         cy.get('li')
-            .contains("phone_mobile is invalid")
-            .should('be.visible')
+          .contains(ERROR_MESSAGES.MISS_FILLING_PHONE_NUMBER)
+          .should('be.visible')
     }
 }
-const fillRequiredFields = (firstName,
-    lastName,
-    email,
-    password,
-    address1,
-    city,
-    state_Id,
-    postalCode,
-    country_Id,
-    mobilePhone,
-    aliasAddress) => {
-    cy.get(locators.firstName)
-        .clear({ force: true })
-        .type(firstName)
-        .should('have.value', firstName)
-    cy.get(locators.lastName)
-        .clear({ force: true })
-        .type(lastName)
-        .should('have.value', lastName)
-    cy.get(locators.password)
-        .clear({ force: true })
-        .type(password)
-        .should('have.value', password)
-    cy.get(locators.address1)
-        .clear({ force: true })
-        .type(address1)
-        .should('have.value', address1)
-    cy.get(locators.city)
-        .clear({ force: true })
-        .type(city)
-        .should('have.value', city)
-    cy.get(locators.state_Id)
-        .select(state_Id)
-        .should('have.value', `${state_Id - 1}`)
-    cy.get(locators.postalCode)
-        .clear({ force: true })
-        .type(postalCode)
-        .should('have.value', postalCode)
-    cy.get(locators.country_Id)
-        .should('have.value', country_Id)
-    cy.get(locators.mobile_phone)
-        .clear({ force: true })
-        .type(mobilePhone)
-        .should('have.value', mobilePhone)
-    cy.get(locators.alias_address)
-        .clear({ force: true })
-        .type(aliasAddress)
-        .should('have.value', aliasAddress)
+export const fillRequiredFields = (firstName, lastName, email, password, address1, city, stateId, postalCode, countryId, mobilePhone, aliasAddress) => {
+    inputTextField(LOCATORS.firstName,firstName)
+    inputTextField(LOCATORS.lastName,lastName)
+    inputTextField(LOCATORS.password,password)
+    inputTextField(LOCATORS.address1,address1)
+    inputTextField(LOCATORS.city,city)
+    selectFromList(LOCATORS.stateId,stateId)
+    inputTextField(LOCATORS.postalCode,postalCode)
+    cy.get(LOCATORS.countryId)            //By Default there is no other choice to choose 
+      .should('have.value', countryId)
+    inputTextField(LOCATORS.mobilePhone,mobilePhone)
+    inputTextField(LOCATORS.aliasAddress,aliasAddress)
 }
 
 
-export {
-    locators,
-    visitURL,
-    click,
-    type,
-    checkEmailMessageError,
-    checkURL,
-    checkRedColorError,
-    fillOptionalFields,
-    checkIfErrorShown,
-    checkErrorTypeVisable,
-    fillRequiredFields
 
-}
 
